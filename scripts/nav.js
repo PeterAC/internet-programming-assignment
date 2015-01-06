0f9a348a14354cc36dcbd21c3c7c9a9cffce2814
PeterAC <petercornell@me.com>
2015-01-06 15:41:11 +1000

    initial commit

---
+++ scripts/nav.js
@@ -0,0 +1,6 @@
+$(document).ready(function () {
+			$("#nav1").animate({marginLeft:'-20px'}, 200);
+			$("#nav2").animate({marginLeft:'-20px'}, 400);
+			$("#nav3").animate({marginLeft:'-20px'}, 600);
+			$("#nav4").animate({marginLeft:'-20px'}, 800);
+});
